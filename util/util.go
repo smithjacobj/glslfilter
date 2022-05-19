10a8ea62ef34c2776a2583269cac780c8fcc6c09
Jacob Masen-Smith <smith.jacob@gmail.com>
2022-05-19 10:29:06 -0700

    Uniform optimization (#9)
    * Setting uniforms as linearized arrays
    * Write uniforms in one shot instead of iterating

--- util/util.go
+++ util/util.go
@@ -2,20 +2,25 @@ package util
 
 import (
 	"log"
+	"runtime"
 )
 
 func Invariant(i interface{}, extras ...interface{}) {
+	stack := make([]byte, 1024)
+	runtime.Stack(stack, false)
+
 	switch v := i.(type) {
 	case error:
 		if v != nil {
-			log.Fatalln(v)
+			log.Fatalf("%s\n%s\n", v, stack)
 		}
 	case bool:
 		if !v {
+
 			if len(extras) > 0 {
 				log.Fatalln(extras...)
 			}
-			log.Fatalln("boolean invariant failure")
+			log.Fatalf("boolean invariant failure\n%s\n", stack)
 		}
 	}
 }
